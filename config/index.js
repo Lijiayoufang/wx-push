f3103d3be97be1e72dce066b071374e2279cc65e
Lijiayoufang <49097492+Lijiayoufang@users.noreply.github.com>
2022-12-08 17:13:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxfc854b3539f73b98",
+  appsecret: "560f3f7e5fadd2ef7b8b3048c5229f86",
+  template_id: "O-As4vUZO8NObyaDnjFAJfwRwRdKEs46GguthK8KWZw",
+  touser: ["oU4WR6rkO08Mp1Ry26392X5-JDdI","oU4WR6r8XomY3BYmNWUX8Ea2Wwm4"],
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "绍兴",
+  first_date: "2022-04-09",
 };
